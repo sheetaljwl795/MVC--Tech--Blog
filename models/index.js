51d6577c36d53913a2768e0e7156ef57d89e5e23
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-10-09 16:53:07 -0700

    Added routes and add the seeds in the user-seeds

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});  
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+module.exports = { User, Post, Comment };
