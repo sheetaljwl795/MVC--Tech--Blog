9f9e10aef77dc442a5ab788bfb8c2ab6141b0915
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-10-31 18:27:56 -0700

    did some changes

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,19 +1,19 @@
 const { Post } = require('../models');
 
 const postData = [{
-        title: 'Monday Tech Digest',
+        title: 'First blog',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         user_id: 1
 
     },
     {
-        title: 'Tuesday Tech Digest',
+        title: 'Second blog',
         content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         user_id: 2
     },
     {
-        title: 'Wednesday Tech Digest',
-        content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        title: 'Third blog',
+        content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur..',
         user_id: 3
     }
 ];
